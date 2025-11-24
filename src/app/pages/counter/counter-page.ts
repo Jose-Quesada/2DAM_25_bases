750db7a68353708180a5c4601b64863d572326bf
Jose-Quesada <jose.quesada@fernando3martos.com>
2025-11-24 12:38:27 +0100

    24 noviembre

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,19 +1,32 @@
-import { Component} from '@angular/core';
+import { Component, signal} from '@angular/core';
 
 @Component({
   selector: 'counter-page',
 
-  template: `
-  <h1>Contador: {{ counter }}</h1>
-  <h2>CounterPage</h2>
-  <button (click)="increaseBy(1)"> +1 </button>`
+  templateUrl:'./counter-page.html',
+
+  styles:`
+    button{
+      margin: 5px 10px;
+      padding: 5px;
+      width: 75px;
+    }
+  `
 
 })
 
 export class CounterPage  {
-  counter = 20;
+  counter = 10;
+  counterSignal = signal(20);
 
   increaseBy( value:number){
     this.counter += value;
+    // this.counterSignal.set( this.counterSignal() + value);
+    this.counterSignal.update( (current) => current + value);
+  }
+
+  resetCounter(){
+    this.counter = 10;
+    this.counterSignal.set(20);
   }
 }
