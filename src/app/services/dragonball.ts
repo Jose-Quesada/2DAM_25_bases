5005192a6c34418deb366ffb76ddd5787930c30f
Jose-Quesada <jose.quesada@fernando3martos.com>
2025-12-05 11:02:41 +0100

    5 diciembre

--- src/app/services/dragonball.ts
+++ src/app/services/dragonball.ts
@@ -1,16 +1,20 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+    const characters = localStorage.getItem('characters');
+
+    return characters ? JSON.parse(characters) : [];
+}
+
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001},
-    { id: 2, name: 'Vegeta', power: 8000},
-
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   addCharacter(character: Character){
     this.characters.update(
@@ -18,4 +22,9 @@ export class DragonballService {
     )
 
   }
+
+  saveToLocalStorage = effect( () => {
+        localStorage.setItem('characters', JSON.stringify(this.characters()))
+    })
+
 }
